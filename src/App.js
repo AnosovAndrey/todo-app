28dfc1a521d159d7519fffabfc1942477a71201e
Andrey <anosov.andru@gmail.com>
2020-11-18 12:29:45 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from "react";
+import {v4 as uuid} from "uuid";
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [todos, setTodos] = React.useState([]);
+    const [todo, setTodo] = React.useState({
+        id: "",
+        text: ""
+    });
+
+    const addTodo = (todo) => {
+        setTodos([...todos, todo]);
+    }
+
+    const handleChange = (event) => {
+        event.preventDefault();
+        setTodo({...todo, text: event.target.value});
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (todo.text.trim()) {
+            addTodo({...todo, id: uuid()})
+            setTodo({...todo, text: ""})
+        }
+    }
+
+    const handleRemove = (id) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    }
+
+    return (
+        <div className="App">
+            <ul>
+                {
+                    todos.map(todo  =>
+                        <li key={todo.id}>
+                            {todo.text}
+                            <button onClick= {() => handleRemove(todo.id)}>X</button>
+                        </li>
+                    )
+                }
+            </ul>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Input todo:
+                    <input
+                        type="text"
+                        value={todo.text}
+                        onChange={handleChange}
+                    />
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    );
 }
 
 export default App;
